d8d6e7aca4f8dcdecc7a7e7c2a54a09a5e34a480
김병훈 <tenderdabin@naver.com>
2021-01-13 17:20:02 +0900

    in it

---
+++ thymeleaf_02/src/test/java/com/test/thymeleaf_02/Thymeleaf02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.thymeleaf_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thymeleaf02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
